53e81170aa039286f14b8325a0fb837a1e3c8094
Annyjerry <106586215+Annyjerry@users.noreply.github.com>
2026-06-16 07:38:37 +0000

    update

--- server.js
+++ server.js
@@ -100,4 +100,4 @@ app.post('/check-ticket', async (req, res) => {
 
 // --- START THE SERVER ---
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
